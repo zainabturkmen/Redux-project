9c0625d6c6bc968af5608a0ae567885ace0c85f7
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-06-06 20:55:29 +0430

    redux store

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import { CartIcon } from "../icons";
+import { useSelector } from "react-redux";
+
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="nav-center">
+        <h3>redux toolkit</h3>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
