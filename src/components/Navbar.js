ae8326b138eb6e5b693e42295f2c346d734872b6
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-06-06 21:03:03 +0430

    redux store

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,7 +11,7 @@ const Navbar = () => {
         <div className="nav-container">
           <CartIcon />
           <div className="amount-container">
-            <p className="total-amount"></p>
+            <p className="total-amount">0</p>
           </div>
         </div>
       </div>
