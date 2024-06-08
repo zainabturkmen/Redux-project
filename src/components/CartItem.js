4b73fee8f10ca2d1c71c4aad91f74cd1735baf8b
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-06-08 12:31:15 +0430

    redux store

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -5,6 +5,8 @@ const CartItem = ({ id, img, title, price, amount }) => {
   return (
     <article className="cart-item">
       <img src={img} alt={title} />
+      <h4>{title}</h4>
+      <h4 className="item-price"></h4>
     </article>
   );
 };
