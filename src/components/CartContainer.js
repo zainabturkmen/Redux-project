0f6cc3aa655d247412b3d46855e87e6264e013d6
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-06-08 09:44:59 +0430

    redux store

--- src/components/CartContainer.js
+++ src/components/CartContainer.js
@@ -1,7 +1,19 @@
 import React from "react";
 import CartItem from "./CartItem";
+import { useSelector } from "react-redux";
+import { store } from "../store";
 
 const cartContainer = () => {
+  const {cartItems, total, amount} = useSelector((store) => store.cart)
+
+  if (amount < 1) {
+    return <section className="cart">
+      <header>
+        <h2>you are back  </h2>
+        <h4 className="empty-cart"></h4>
+      </header>
+    </section>
+  }
   return <div>cart continw</div>;
 };
 
