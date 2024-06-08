e9fd6e98766152d514c3c875272783719a91b239
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-06-08 12:56:27 +0430

    redux store

--- src/components/CartContainer.js
+++ src/components/CartContainer.js
@@ -1,11 +1,11 @@
 import React from "react";
 import CartItem from "./CartItem";
-import {useDispatch, useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { store } from "../store";
 import { clearCart } from "../features/cart/cartSlice";
 
 const CartContainer = () => {
-  const dispatch =  useDispatch()
+  const dispatch = useDispatch();
   const { cartItems, total, amount } = useSelector((store) => store.cart);
 
   if (amount > 1) {
@@ -35,7 +35,9 @@ const CartContainer = () => {
             total <span>${total}</span>
           </h4>
         </div>
-        <button className="btn clear-btn" onClick={() => dispatch(clearCart())}>clear cart</button>
+        <button className="btn clear-btn" onClick={() => dispatch(clearCart())}>
+          clear cart
+        </button>
       </footer>
     </section>
   );
