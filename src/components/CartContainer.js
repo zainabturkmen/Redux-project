cca58def9f0017cb1f5ad522975f27dd4cf80832
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-06-08 11:51:57 +0430

    redux store

--- src/components/CartContainer.js
+++ src/components/CartContainer.js
@@ -4,21 +4,30 @@ import { useSelector } from "react-redux";
 import { store } from "../store";
 
 const cartContainer = () => {
-  const {cartItems, total, amount} = useSelector((store) => store.cart)
+  const { cartItems, total, amount } = useSelector((store) => store.cart);
 
-  if (amount < 1) {
-    return <section className="cart">
-      <header>
-        <h2>your back  </h2>
-        <h4 className="empty-cart">is currently empty</h4>
-      </header>
-    </section>
+  if (amount > 1) {
+    return (
+      <section className="cart">
+        <header>
+          <h2>your back </h2>
+          <h4 className="empty-cart">is currently empty</h4>
+        </header>
+      </section>
+    );
   }
-  return <section className="cart">
-    <header>
-      <h2>your bag</h2>
-    </header>
-  </section>
+  return (
+    <section className="cart">
+      <header>
+        <h2>your bag</h2>
+      </header>
+      <div>
+        {cartItems.map((item) => {
+          return <CartItem key={item.id} {...item} />;
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default cartContainer;
