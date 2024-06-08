2febacae26b7bd68855d308cd219020617920021
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-06-08 12:25:22 +0430

    redux store

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,6 +1,7 @@
 import React from "react";
+import { ChevronDown, ChevronUp } from "../icons";
 
-const CartItem = () => {
+const CartItem = ({id, img, title, price, amount}) => {
   return <div>cart cartItem</div>;
 };
 
