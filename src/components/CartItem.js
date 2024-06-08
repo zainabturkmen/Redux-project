c7036fe3331bfd9b460c9d952571b2611c8a975d
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-06-08 12:35:22 +0430

    redux store

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -10,6 +10,15 @@ const CartItem = ({ id, img, title, price, amount }) => {
         <h4 className="item-price">${price}</h4>
         <button className="remove-btn">remove</button>
       </div>
+      <div>
+        <button className="amount-btn">
+          <ChevronUp />
+        </button>
+        <p className="amount">{amount}</p>
+        <button className="amount-btn">
+          <ChevronDown />
+        </button>
+      </div>
     </article>
   );
 };
