0bb2efaaf9c7d932439c77a739d9277b4bb6a49c
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-06-09 10:07:08 +0430

    redux store

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -5,7 +5,7 @@ import { act } from "@testing-library/react";
 const url = "https://course-api.com/react-useReducer-cart-project";
 
 const initialState = {
-  cartItems: cartItems,
+  cartItems: [],
   amount: 0,
   total: 0,
   isLoading: true,
@@ -49,19 +49,18 @@ const cartSlice = createSlice({
       state.total = total;
     },
   },
-  extraReducers:{
-    [getCartItems.pending]:(state) => {
-      state.isLoading = true
+  extraReducers: {
+    [getCartItems.pending]: (state) => {
+      state.isLoading = true;
     },
-    [getCartItems.fulfilled]:(state, action) => {
+    [getCartItems.fulfilled]: (state, action) => {
       console.log(action);
-      state.isLoading = false,
-      state.cartItems = action.payload
+      (state.isLoading = false), (state.cartItems = action.payload);
     },
-    [getCartItems.rejected]:(state) => {
-      state.isLoading = false
+    [getCartItems.rejected]: (state) => {
+      state.isLoading = false;
     },
-  }
+  },
 });
 console.log(cartSlice);
 
