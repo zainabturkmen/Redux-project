96b01ede55eaeb900f3c49618ec84c89589511ea
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-06-09 07:39:08 +0430

    redux store

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import Navbar from "./Components/Navbar";
 import CartContainer from "./Components/CartContainer";
+import { useDispatch } from "react-redux";
+import { calculateTotal } from "./features/cart/cartSlice";
 
 function App() {
   return (
