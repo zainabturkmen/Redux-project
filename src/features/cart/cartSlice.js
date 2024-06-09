12f15f79aaae2535677724f036aa09b6948288d0
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-06-09 09:31:52 +0430

    redux store

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -48,6 +48,11 @@ const cartSlice = createSlice({
       state.total = total;
     },
   },
+  extraReducers:{
+    [getCartItems.pending]:(state) => {
+      state.isLoading = true
+    }
+  }
 });
 console.log(cartSlice);
 
