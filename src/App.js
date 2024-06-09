54fd54948cf90e20d0e3c878219e2db6e20a29a6
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-06-09 07:39:46 +0430

    redux store

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import Navbar from "./Components/Navbar";
 import CartContainer from "./Components/CartContainer";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { calculateTotal } from "./features/cart/cartSlice";
 
 function App() {
+  const { cartItems } = useSelector();
   return (
     <main>
       <Navbar />
