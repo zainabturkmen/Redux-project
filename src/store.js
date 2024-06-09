9f195b3d0ca2804ec0fc018784f9bdd575b6ed28
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-06-09 08:03:21 +0430

    redux store

--- src/store.js
+++ src/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./features/cart/cartSlice";
+import ModalReducer from "./features/Modal/ModalSlice";
 
 export const store = configureStore({
   reducer: {
     cart: cartReducer,
+    modal: ModalReducer,
   },
 });
