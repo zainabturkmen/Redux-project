8d3eb5227b5d359d9eff3fa8c6a8b241abb8c33d
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-06-09 07:52:10 +0430

    redux store

---
+++ src/components/Modal.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Modal = () => {
+  return (
+   <aside className='modal-container'>
+    <div className='modal'>
+        <h4>remove all items from your shopping cart?</h4>
+        <div className='btn-container'>
+            <button className='btn confirm-btn' type='button'></button>
+        </div>
+    </div>
+   </aside>
+  );
+}
+
+export default Modal;
